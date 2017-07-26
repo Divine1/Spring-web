48713d21f3ef6053203553182fb56bb9d912855f
Divine <cdivine304@gmail.com>
2017-07-26 16:11:04 +0530

    For spring security documentation issue reference1

---
+++ springsecurityTest1/setup-security-xml/src/main/java/com/rw/HomeController.java
@@ -0,0 +1,24 @@
+package com.rw;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HomeController {
+
+	
+	@RequestMapping(value = {"/","/welcome"})
+	public String index(){
+		return "welcome";
+	}
+	
+	@RequestMapping(value="/courses")
+	public String courses(){
+		return "courses";
+	}
+	
+	@RequestMapping(value="/login")
+	public String login(){
+		return "login";
+	}
+}
